d443788b0f4fae0813d4866f0421ee4f9a5d81bd
David A Castro <83044518+dcastro2021@users.noreply.github.com>
2021-05-18 20:33:10 -0500

    Add files via upload

---
+++ FinalProject_Unit1.py
@@ -0,0 +1,34 @@
+#Final Project
+print("This is my Final Project - Console Application")
+namechoice=input("Please input desired keywored to rename all files in subfolders or folder")
+
+
+#Rename all files in subfolders of folder
+import os
+
+directoryName = "TEST FOLDER FOR FINAL PROJECT UNIT I"
+filePath = ("C:\\\\Users\\bodhi\\SAVVYCODE\\DAPSupplementalCode\\TESTFOLDER1\\")
+print(filePath)
+
+
+files = os.listdir(filePath)
+filePathWithSlash = filePath + "\\"
+print(files)
+
+for counter, filename in enumerate(os.listdir(filePath)):
+
+    filenameWithPath = os.path.join(filePathWithSlash, "Test file I")
+
+    split_file = os.path.splitext(filename)
+    os.rename(filePath + filename, filePath + namechoice + str(counter) + split_file[1])
+
+    print ("Successfully renamed.")
+
+
+
+
+
+#Range, #list of file or path locations
+#2nd loop with the range, for x in range 10.and
+
+
